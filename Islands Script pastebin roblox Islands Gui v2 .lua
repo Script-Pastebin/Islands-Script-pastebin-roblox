d5c1094b4362d72949bcdabba9ee8dac6400d9d5
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:33:28 +0100

    commit zero

---
+++ Islands Script pastebin roblox Islands Gui v2 .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://system-exodus.com/scripts/Islands/Islands.lua',true))(
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script islands.It has: Auto-Farm, Teleports, Fly and God mode and much more!
+						
+]]
